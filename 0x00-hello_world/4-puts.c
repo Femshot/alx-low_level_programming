57f98994dfb5d70fe5c8c81449d11c5f63e3b905
Femshot <awodelefemi@yahoo.com>
2022-09-08 08:08:17 -0700

    first betty styled c-code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Print statement - Print "Programming is like building a multilingual puzzle"
+ *
+ * main - entry point
+ *
+ * Return: 0 after printing statement
+ */
+int main(void)
+{    puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
