aa239d96cbe321867447dd12e9451c8e36df046d
Femshot <awodelefemi@yahoo.com>
2022-09-08 08:32:27 -0700

    2nd betty stylecode

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{    printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
