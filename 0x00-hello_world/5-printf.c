50b8716b9e612427047b7643378b2d9d566c8ff1
Femshot <awodelefemi@yahoo.com>
2022-09-08 08:33:54 -0700

    2nd betty stylecode

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * main - prints "with proper grammar, but the outcome is a piece of art,\n"
  *
  * Return: 0 (success)
  */
