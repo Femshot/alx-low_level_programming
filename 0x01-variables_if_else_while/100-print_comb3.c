806085c812e77a9355deb9e62802063df7f447c4
Femshot <awodelefemi@yahoo.com>
2022-09-09 13:50:38 -0700

    how far can you go

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+	for (b = '0'; b <= '9'; b++)
+	{
+		if (a != b)
+	{
+		putchar(a);
+		putchar(b);
+
+		if (d == '8' && p == '9')
+		continue;
+
+		putchar(',');
+		pitchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
