89f9e358584ca1268efef131561ea4f4ca2506ba
Femshot <awodelefemi@yahoo.com>
2022-09-13 04:04:22 -0700

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * main - Prints word _putchar followed by new line
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 1; i <= 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Repo's readme
