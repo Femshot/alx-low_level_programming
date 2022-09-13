c87562ed5c54c5eff1b4093510bb76e79a041bfb
Femshot <awodelefemi@yahoo.com>
2022-09-13 07:26:00 -0700

    first without main.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute in day from 00:00 to 23:59.
+ *
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
