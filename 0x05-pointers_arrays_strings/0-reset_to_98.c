cb3832cfbc06de00196170cbce82974579a5f2be
root <root@Awodele-Femi.localdomain>
2022-09-19 17:47:33 +0100

    changed a to n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * reset_to_98 - changes value of int to a pointer to 98
- * 'a' - A pointer
+ * 'n' - A pointer
  * &n - address of variable held be pointer A
  */
 
 void reset_to_98(int *n)
 {
-	int *n = &n;
-
-	*a = 98;
+	*n = 98;
 }
