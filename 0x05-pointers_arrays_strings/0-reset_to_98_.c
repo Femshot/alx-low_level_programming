0e1336cd19a0b06f76d7ab4eb566369d623f2706
root <root@Awodele-Femi.localdomain>
2022-09-19 17:33:29 +0100

    first commit - changing pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98_.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - changes value of int to a pointer to 98
+ * 'a' - A pointer
+ * &n - address of variable held be pointer A
+ */
+
+void reset_to_98(int *a)
+{
+	int *a = &n;
+
+	*a = 98;
+}
