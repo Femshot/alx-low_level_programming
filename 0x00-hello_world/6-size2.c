5d59de1505279f501beb811c4275c45076799c93
root <root@Awodele-Femi.localdomain>
2022-09-20 11:09:04 +0100

    finally 8te

---
+++ 0x00-hello_world/6-size2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - sizes of various types
+ *
+ * Return: 0 after prinf of info
+ */
+int main(void)
+{
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+return (0);
+}
