92607e53a8ce9b57eba3a2e09d5a5f94202da2a0
root <root@Awodele-Femi.localdomain>
2022-09-21 00:34:40 +0100

    fibonacci 3

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned int fibonacci[32], sum = 2;
+	int n;
+
+	fibonacci[1] = 1;
+	fibonacci[2] = 2;
+
+	for(n = 3; n <= 32; n++)
+	{
+		fibonacci[n] = fibonacci[n - 1] + fibonacci[n - 2];
+		if(fibonacci[n] % 2 == 0)
+			sum += fibonacci[n];
+	}
+	printf("%u\n", sum);
+
+	return (0);
+}
