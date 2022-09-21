d359ff3693e0d83cb728d9813fff75cecf583b9b
root <root@Awodele-Femi.localdomain>
2022-09-21 20:09:49 +0100

    main.h ammended

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char b);
-char *_strcat(char *des, char *src);
-char *_strncat(char *dest, char *srcs, int n);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
