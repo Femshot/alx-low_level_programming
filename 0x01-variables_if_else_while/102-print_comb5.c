8d868ca527beb1cd142d2ed265d81fa0421769f8
root <root@Awodele-Femi.localdomain>
2022-09-21 20:02:01 +0100

    first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point: prints all posible combinations of two two-digit numbers
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for(a = '0', a <= '9', a++);
+	{
+		for 
