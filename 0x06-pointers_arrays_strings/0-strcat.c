0fab9c0e96d093af8a2845b945f81c6e48021405
root <root@Awodele-Femi.localdomain>
2022-09-21 20:01:23 +0100

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - Appends the string stored in *src into the string in *dest
+ * @dest: an input string
+ * @src: an input string
+ * Return: Address of resulting string
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char b);
+char *_strcat(char *des, char *src);
+char *_strncat(char *dest, char *srcs, int n);
+
+#endif
