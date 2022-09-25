53cdcd871352a048fb903acffb9c2967cea2665e
root <root@Awodele-Femi.localdomain>
2022-09-25 23:17:39 +0100

    0x0=0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,10 +10,11 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n >= 1 && n < 15)
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			if (n != 0)
 			_putchar('0');
 			for (j = 1; j <= n; j++)
 				putformat(i * j);
