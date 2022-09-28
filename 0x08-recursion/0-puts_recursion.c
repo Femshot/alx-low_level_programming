2257eab3fb1fe5845700c3fb1f9f66bfa464db72
root <root@Awodele-Femi.localdomain>
2022-09-28 14:56:57 +0100

    about to be recursive

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *_puts_recursion - prints a string followed by a new line
+ *@s: string to be printed
+ *Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar (*s);
+		_puts_recursion(char *(s + 1));
+	}
+}
