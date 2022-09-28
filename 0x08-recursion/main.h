fc709ac80fc0c59aae943e755960e35f1e56aace
root <root@Awodele-Femi.localdomain>
2022-09-28 16:42:15 +0100

    5 & 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,9 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt(int prev, int root);
+int _sqrt_recursion(int n);
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
 #endif
