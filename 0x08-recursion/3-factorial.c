5358e4efed26d99bec2a93deae7e716a2ade08e3
root <root@Awodele-Femi.localdomain>
2022-09-28 16:06:28 +0100

    3 again

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,19 +2,23 @@
 
 /**
  * factorial - computes factorial of a given number
- * @n - number to cumpute
- * return: factorial of n computed if n is greater than 0 or 0 if n is 0
+ * @n: number to compute
+ * Return: factorial of n computed if n is greater than 0 or 0 if n is 0
  * or -1 if n is less than 0
  */
 
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 	else if (n == 0)
+	{
 		return (1);
+	}
 	else
 	{
-		return (n * factorial(n-1));
+		return (n * factorial(n - 1));
 	}
 }
