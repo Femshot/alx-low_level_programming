99f2969b4f53e7a85b860bdceeef9d550c325a06
Femshot <awodelefemi@yahoo.com>
2022-11-01 16:18:06 +0100

    corrdctions to .h

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -5,6 +5,7 @@
  * @head: A pointer to listint_t structure
  * Return: The address of the node where the loop start, or NULL
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *one = head, *two = head;
