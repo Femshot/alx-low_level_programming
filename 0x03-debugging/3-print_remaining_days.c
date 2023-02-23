95971f0f0eb247bb6c35c17181fa78f48f3a888b
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-02-23 03:24:50 +0100

    02/20/2000 is day 60 in month 2, month has to be > 2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,11 +20,10 @@ void print_remaining_days(int month, int day, int year)
 
 	if ((test1 || test2) && (test3 || test4) && test5)
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
