752694533232804c9a557f67b53b54a5a6dd77a9
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-02-27 18:40:53 +0100

    tasks 4, and 3 again

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: Pointer to string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	char *f = s;
+
+	while (*s++ != '\0')
+		i++;
+	s = f; /*takes s back to starting point*/
+	for (; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
