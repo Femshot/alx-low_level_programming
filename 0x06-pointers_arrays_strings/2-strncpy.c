98bca0aa7e1bac2a2fe77e32d5d75226bfa8d08c
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-04 00:32:48 +0100

    adjusted to iterate as pointer incrementing

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,11 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	char *ptr = dest;
 
-	for (; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	for (; i < n && *(src +i) != '\0'; i++)
+		*(dest + i) = *(src + i);
 
-	for (; i < n && dest[i]; i++)
-		dest[i] = '\0';
+	for (; i < n && *(dest + i); i++)
+		*(dest + i) = '\0';
 
 	return (ptr);
 }
