c6ef91ef7d3f8a9ce140505b974bc6dde8871e54
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-06 12:42:00 +0100

    Redame and task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _memset - Fills a memory location with consatn byte up to a certain number
@@ -9,42 +8,6 @@
  *
  * Return: Pointer to memory thats been filled
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
-
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
-}
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
