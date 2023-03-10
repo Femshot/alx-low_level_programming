965800aade548f26170e676468439775dc03aff3
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-10 23:41:36 +0100

    the greatest addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Prints all arguments passed into it
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
     {
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0)
+		if (isdigit(*argv[i]))
 		{
 			add += atoi(argv[i]);
 		}
