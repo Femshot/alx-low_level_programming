f7ab1e7f445f92c12eb03fa65df853a1b8f26356
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-13 16:14:39 +0100

    NULL not '\0' is to return NULL

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,12 @@ char *_strdup(char *str)
 	char *ptr;
 	int i, len = 0;
 
+	if (str == NULL)
+		return (NULL);
 	while (*(str + len))
 		len++;
 	ptr = (char *)malloc((len + 1) * sizeof(char));
-	if (len == 0 || ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
 		*(ptr + i) = *(str + i);
