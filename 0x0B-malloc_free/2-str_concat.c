c672d33f5bb92e43838b91d3f14897bb3a664a8a
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-13 16:23:12 +0100

    NULL treated aas '\0' or empty string not failed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,21 +9,24 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0, i, j;
+	int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *ptr;
 
 	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	while (*(s1 + len1))
+		len1 = len2 = 0;
+	else
+	{
+		while (*(s1 + len1))
 		len1++;
-	while (*(s2 + len2))
+		while (*(s2 + len2))
 		len2++;
+	}
 	ptr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-	if (ptr == NULL || len1 == 0 || len2 == 0)
+	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	for (; i < len1 && *(s1 + i); i++)
 		*(ptr + i) = *(s1 + i);
-	for (j = 0; j < len2; j++, i++)
+	for (; j < len2 && *(s2 + j); j++, i++)
 		*(ptr + i) = *(s2 + j);
 	*(ptr + i) = '\0';
 	return (ptr);
