7987edcd4514789725bcc8c1c66055b4e520678c
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-13 20:16:16 +0100

    freeing array before return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,6 +27,8 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		for (; j < width; j++)
 			array[i][j] = 0;
-
+	for (i = 0; i < height; i++)
+		free((int *)array[i]);
+	free(array);
 	return (array);
 }
