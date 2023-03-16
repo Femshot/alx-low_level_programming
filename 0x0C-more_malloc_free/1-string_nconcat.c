21d111cdde1f15733188826aa7608e83dd9504f5
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-16 12:57:37 +0100

    Added guard against checking NULL string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,10 +18,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len1 = 0;
 	if (s2 == NULL)
 		len2 = 0;
-	while (*(s1 + len1))
-		len1++;
-	while (*(s2 + len2))
-		len2++;
+	if (s1 != NULL)
+		while (*(s1 + len1))
+			len1++;
+	if (s2 != NULL)
+		while (*(s2 + len2))
+			len2++;
 	if (n >= len2)
 		n = len2;
 
