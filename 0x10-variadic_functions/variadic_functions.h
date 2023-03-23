45453ea808108413e3258870b58a6e5cfe79813e
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-03-24 00:17:09 +0100

    Reverted alternate solution changes, needs a bit more work

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,21 +9,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
- * struct frmt - Struct for print_all function
- * @c: A character
- * @f: Pointer to function tht takes one argument
- */
-typedef struct frmt
-{
-	char c;
-	void (*f)(va_list);
-} fmt;
-
-void print_char(va_list args);
-void print_int(va_list args);
-void print_float(va_list args);
-void print_string(va_list args);
-void (*check(char s))(va_list);
-
 #endif
