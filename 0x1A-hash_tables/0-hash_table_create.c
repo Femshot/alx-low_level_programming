c16e872f7437e914cad9c8591d6bd42c4715c53b
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-06-09 20:17:21 +0100

    Removed stray code lines to clear betty error in tsks 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -25,14 +25,3 @@ hash_table_t *hash_table_create(unsigned long int size)
 		(table->array)[a] = NULL;
 	return (table);
 }
-
-
-
-int main(void)
-{
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
-}
